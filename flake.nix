dedfd308fe347c63a353c8715bde14b6cb9b1125
Dustin Krysak <dustin@bashfulrobot.com>
2026-07-29 22:42:39 -0700

    Add flake.nix: dev shell + installable package
    - packages.default / packages.snowstorm: buildGoModule derivation,
      so other flakes can consume this as an input (packages.<system>.default)
    - overlays.default: exposes pkgs.snowstorm for overlay-style consumption
    - apps.default: nix run
    - devShells.default: go, gopls, gotools, golangci-lint, gofumpt, delve

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "snowstorm - Snowflake data-access CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        snowstorm = pkgs.buildGoModule {
+          pname = "snowstorm";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = "sha256-ZLhEXSCjePBQsqW8ZpnA8c7kX9wu4Qh51HS0M7TQc2w=";
+
+          meta = with pkgs.lib; {
+            description = "Snowflake data-access CLI: run queries, get structured JSON back";
+            homepage = "https://github.com/bashfulrobot/snowstorm";
+            license = licenses.mit;
+            mainProgram = "snowstorm";
+          };
+        };
+      in
+      {
+        packages = {
+          default = snowstorm;
+          snowstorm = snowstorm;
+        };
+
+        apps.default = flake-utils.lib.mkApp { drv = snowstorm; };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            golangci-lint
+            gofumpt
+            delve
+          ];
+
+          shellHook = ''
+            echo "snowstorm dev shell: $(go version)"
+          '';
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    )
+    // {
+      overlays.default = final: _prev: {
+        snowstorm = self.packages.${final.stdenv.hostPlatform.system}.default;
+      };
+    };
+}
